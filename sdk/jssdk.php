c6077f081627e88700ceae5ef46043220e0b870b
xueyi <xueyi@qq.com>
2017-06-26 17:34:20 +0800

    update jssdk.php

--- sdk/jssdk.php
+++ sdk/jssdk.php
@@ -116,6 +116,6 @@ $appSecret = '97b9ca867ce04dbee22661dde3ceb3f8';
 
 $jssdk = new JSSDK($appId,$appSecret);
 $arr = $jssdk->getSignPackage();
-$arr = json_encode($arr);
-return $arr;
+echo json_encode($arr);
+
 
