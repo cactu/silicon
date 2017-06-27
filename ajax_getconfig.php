5245f50717b8963e74271a861edee6b9b03c6814
xueyi <xueyi@qq.com>
2017-06-27 11:25:45 +0800

    over

--- ajax_getconfig.php
+++ ajax_getconfig.php
@@ -1,6 +1,6 @@
 <?php
 	require_once "jssdk.php";
-	$jssdk = new JSSDK("wx36f784e8c13faff0","97b9ca867ce04dbee22661dde3ceb3f8");
+	$jssdk = new JSSDK("wx36f784e8c13faff0","97b9ca867ce04dbee22661dde3ceb3f8",'$_GET['url']');
 	$signPackage = $jssdk->GetSignPackage();
 	$signPackage['jsApiList'] = array('onMenuShareTimeline','onMenuShareAppMessage');
 	$signPackage['debug'] = true;
