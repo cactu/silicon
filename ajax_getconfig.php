fe7c38deca5b373ac14eea8730fad19f65a4fdbf
xueyi <xueyi@qq.com>
2017-06-27 11:11:23 +0800

    add ajax

--- ajax_getconfig.php
+++ ajax_getconfig.php
@@ -1,5 +1,5 @@
 <?php
-	require_once "jssdk.php";
+	require_once "./ceshi/jssdk.php";
 	$jssdk = new JSSDK("wx36f784e8c13faff0", "97b9ca867ce04dbee22661dde3ceb3f8");
 	$signPackage = $jssdk->GetSignPackage();
 	$signPackage['jsApiList'] = array('onMenuShareTimeline','onMenuShareAppMessage');
