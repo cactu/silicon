68a159eacf90eabb20d6b06c2308286a9a002d91
xueyi <xueyi@qq.com>
2017-06-27 09:49:00 +0800

    add

--- ceshi/sample.php
+++ ceshi/sample.php
@@ -1,6 +1,6 @@
 <?php
 require_once "jssdk.php";
-$jssdk = new JSSDK("yourAppID", "yourAppSecret");
+$jssdk = new JSSDK("wx36f784e8c13faff0", "97b9ca867ce04dbee22661dde3ceb3f8");
 $signPackage = $jssdk->GetSignPackage();
 ?>
 <!DOCTYPE html>
