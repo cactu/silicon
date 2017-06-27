07aea7b9ec19cc0ff6d197f6e226afc5dfc8ad43
xueyi <xueyi@qq.com>
2017-06-27 09:47:45 +0800

    add ceshi

--- sdk/access_token.php
+++ sdk/access_token.php
@@ -1,1 +1,1 @@
-<?php exit();?>{"access_token":"2ioGgNv40SfdaUwd2WCdAghilBd5j5_wQpOuh1037b3UNoV0lLVZGz6JygEzzQlc5Fl4Tf3X54OlhAc1F2H0sfJ6Ky6ehM4CTgSXC6GMMpUsh_FgnvAQ1H9LHVd-R448DCUeADAZYQ","expire_time":1498472755}
+<?php exit();?>{"access_token":"1OPD2WqC8yH8t2x0etIMo1G_1hwkRyaxNFudpCflNXgyyF1oAkeyhZYQ1ZpUc99WQfSJ4EmGTPFW08y5PRJIJGFk2LmMtXRGqyV98A5x0DQj39zpuvbvf9jb9shQnk3GQNBcAJAGDR","expire_time":1498480063}
